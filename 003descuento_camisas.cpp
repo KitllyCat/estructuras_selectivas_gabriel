6a0ce09e825e14d05c8e9b393e8e4fcb7dfbca87
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-05-30 23:33:03 -0500

    Añadiendo una validación para evitar valores negativos o cero

--- 003descuento_camisas.cpp
+++ 003descuento_camisas.cpp
@@ -13,7 +13,11 @@ int main() {
     cin >> cantidad;
     cout << "Precio unitario: S/.";
     cin >> precio;
-    
+
+    if (cantidad <= 0 || precio <= 0) {
+        cout << "La cantidad de camisas o el precio de estas no pueden ser cero o negativas." << endl;
+        return 1;
+    }
     total = cantidad * precio;
     if(cantidad >= 3){
         descuento = total * 0.2;
@@ -22,7 +26,7 @@ int main() {
         descuento = total * 0.1;
         total = total - descuento;
     }
-
     cout << "Total a pagar: S/." << total;
+
     return 0;
 }
