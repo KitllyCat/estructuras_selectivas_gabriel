3acb550d413fdfbaece57f198db5df04a23225d3
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-05-31 22:59:23 -0500

    Agregando validaciones para los datos de entrada y mensajes de error

--- 004promocion_supermercado.cpp
+++ 004promocion_supermercado.cpp
@@ -5,20 +5,29 @@ using namespace std;
 
 int main() {
     float totalCompra;
-	float descuento;
-	float totalPagar;
+    float descuento;
+    float totalPagar;
     int numero;
 
-    cout << "Ingrese el monto total de la compra: ";
-    cin >> totalCompra;
-    cout << "Ingrese un numero (0-100): ";
-    cin >> numero;
-
+    do {
+        cout << "Ingrese el monto total de la compra: ";
+        cin >> totalCompra;
+        if (totalCompra <= 0) {
+            cout << "El monto debe ser mayor a 0" << endl;
+        }
+    } while (totalCompra <= 0);
+    do {
+        cout << "Ingrese un numero (0-100): ";
+        cin >> numero;
+        if (numero < 0 || numero > 100) {
+            cout << "El numero tiene que estar entre 0 y 100" << endl;
+        }
+    } while (numero < 0 || numero > 100);
     if (numero < 74){
         descuento = totalCompra * 0.15;
     }else{
         descuento = totalCompra * 0.20;
-	}
+    }
     totalPagar = totalCompra - descuento;
 
     cout << "\nTotal a pagar: S/." << totalPagar << endl;
