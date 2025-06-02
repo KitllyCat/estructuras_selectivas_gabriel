e6af6a4cb94d56f4b4aeeacf4c4f680003b05001
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-06-02 14:12:26 -0500

    Añadiendo dos validaciones para que los datos de entrada sean correctos

--- 006descuento_pension.cpp
+++ 006descuento_pension.cpp
@@ -13,6 +13,14 @@ int main() {
     cout << "Ingrese el monto de la pensión: ";
     cin >> pension;
 
+    if (promedio < 0 || pension > 20) {
+        cout << "El promedio debe estar entre 0 y 20" << endl;
+        return 1;
+    }
+    if (pension <= 0) {
+        cout << "El monto de la pensión debe ser mayor a 0" << endl;
+        return 1;
+    }
     if (promedio >= 18) {
         total = pension * 0.70;
     }else{
